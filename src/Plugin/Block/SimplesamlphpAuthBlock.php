49d8dd344842ccafa87a18a037d40a84b38df9e1
Sven Decabooter <sdecabooter@gmail.com>
2016-02-01 09:47:46 +0100

    Issue #2658768 by svendecabooter: Errors in SimplesamlphpAuthBlock

--- src/Plugin/Block/SimplesamlphpAuthBlock.php
+++ src/Plugin/Block/SimplesamlphpAuthBlock.php
@@ -10,44 +10,93 @@ namespace Drupal\simplesamlphp_auth\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Routing\LinkGeneratorTrait;
 use Drupal\Core\Url;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\simplesamlphp_auth\Service\SimplesamlphpAuthManager;
+use Drupal\Core\Config\ConfigFactoryInterface;
 
 /**
- * Provides a 'Most recent poll' block.
+ * Provides a 'SimpleSAMLphp authentication status' block.
  *
  * @Block(
  *   id = "simplesamlphp_auth_block",
  *   admin_label = @Translation("SimpleSAMLphp Auth Status"),
  * )
  */
-class SimplesamlphpAuthBlock extends BlockBase {
+class SimplesamlphpAuthBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
   use LinkGeneratorTrait;
+
+  /**
+   * SimpleSAMLphp Authentication helper.
+   *
+   * @var SimplesamlphpAuthManager
+   */
+  protected $simplesamlAuth;
+
+  /**
+   * A configuration object.
+   *
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected $config;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('simplesamlphp_auth.manager'),
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * Creates a LocalActionsBlock instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param SimplesamlphpAuthManager $simplesaml_auth
+   *   The SimpleSAML Authentication helper service.
+   * @param ConfigFactoryInterface $config_factory
+   *   The configuration factory.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, SimplesamlphpAuthManager $simplesaml_auth, ConfigFactoryInterface $config_factory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->simplesamlAuth = $simplesaml_auth;
+    $this->config = $config_factory->get('simplesamlphp_auth.settings');
+
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-
-    $activated = \Drupal::config('simplesamlphp_auth.settings')->get('activate');
-
-    $simplesaml = \Drupal::service('simplesamlphp_auth.manager');
-
-    if ($activated) {
-      if ($simplesaml->isAuthenticated()) {
-        $content = $this->t('Logged in as %authname<br />!logout', array(
-          '%authname' => $simplesaml->getAuthname(),
-          '!logout' => $this->l('Log Out', new Url('user.logout')),
+    if ($this->simplesamlAuth->isActivated()) {
+      if ($this->simplesamlAuth->isAuthenticated()) {
+        $content = $this->t('Logged in as %authname<br />:logout', array(
+          '%authname' => $this->simplesamlAuth->getAuthname(),
+          ':logout' => $this->l('Log Out', new Url('user.logout')),
         ));
       }
       else {
-        $label = Drupal::config('simplesamlphp_auth.settings')->get('login_link_display_name');
-        $content = $this->t('!login', array(
-          '!login' => $this->l($label, new Url('simplesamlphp_auth.saml_login')),
-        ));
+        $label = $this->config->get('login_link_display_name');
+        $content = $this->l($label, new Url('simplesamlphp_auth.saml_login', array(), array(
+          'attributes' => array(
+            'class' => array('simplesamlphp-auth-login-link'),
+          ),
+        )));
       }
     }
     else {
-      $content = $this->t('SimpleSAML not enabled');
+      $content = $this->t('Warning: SimpleSAMLphp is not activated.');
     }
 
     return array(
